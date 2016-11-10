819238367a1b58f84831cb2733564fd4b045799b
Igors Sigalovs <back2photoshop@gmail.com>
2016-11-10 11:55:19 +0000

    Added gravity button test

--- app/src/main/java/pt/ipbeja/a14103/uiexerciseone/MainActivity.java
+++ app/src/main/java/pt/ipbeja/a14103/uiexerciseone/MainActivity.java
@@ -1,10 +1,33 @@
 package pt.ipbeja.a14103.uiexerciseone;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int posIndex = 0;
+    private int [] positions = new int []
+            {
+                    Gravity.TOP | Gravity.RIGHT,
+                    Gravity.BOTTOM | Gravity.RIGHT,
+                    Gravity.BOTTOM | Gravity.LEFT,
+                    Gravity.TOP | Gravity.LEFT,
+
+            };
+
+    public void btnNextPosition_onClick(View view)
+    {
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        if(posIndex >= this.positions.length)
+            posIndex = 0;
+        posIndex++;
+        ActionBar.LayoutParams var = new ActionBar.LayoutParams(this.positions[this.posIndex]);
+        img.setLayoutParams(var);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
